b4c797da207406574fd6ad08ff7ca68c84816f94
Rashintha Maduneth <rashinthamaduneth@gmail.com>
2020-10-09 21:58:51 +0530

    Enigma folder structure is created.

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    /*
+     * Enigma Framework
+     * 
+     */
+
+    define('enigma');
+
+    include_once 'configurations.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Enigma Framework</title>
+</head>
+</html>
